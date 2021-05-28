e466f3fd316d1f75640a5353ba6e3e4822598518
gowthampsg96 <gowthamrajendran1996@gmail.com>
2021-05-28 17:45:20 +0530

    first commit

--- app.py
+++ app.py
@@ -6,6 +6,20 @@ app = Flask(__name__)
 def index():
     return "HELLO WORLD"
 
+@app.route('/check-palindrome')
+def palindrome():
+    value = request.args.get('value')
+    print(value)
+    if 'count' in session:
+        session['count'] = session.get('count') + 1
+    else:
+        session['count'] = 1
+    print(session.get('count'))
+    # return "Total visit: {}".format(session.get('count'))
+    for i in range(0, int(len(value) / 2)):
+        if value[i] != value[len(value) - i - 1]:
+            return "Not a palindrome"
+    return '''palindrome: {}'''.format(value)
 
 @app.route('/')
 def check():
