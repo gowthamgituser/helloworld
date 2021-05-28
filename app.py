68be70f29269ab268689eeab21db5da7204e7c0a
gowthampsg96 <gowthamrajendran1996@gmail.com>
2021-05-28 17:12:20 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-apps = Flask(__name__)
+app = Flask(__name__)
 @app.route('/')
 def check():
     return "CHECK RUN"
 
 
 if __name__ == '__main__':
-    apps.run()
+    app.run()
