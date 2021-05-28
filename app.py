6251d7d1a3a2a9d19880c8d99f98c30eed07a85b
gowthampsg96 <gowthamrajendran1996@gmail.com>
2021-05-28 17:43:10 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/hello-world', methods=['GET'])
+def index():
+    return "HELLO WORLD"
+
+
 @app.route('/')
 def check():
     return "CHECK RUN"
