6dc107de8dba9b1b02aa59d46c324e71ddab5e7c
gowthampsg96 <gowthamrajendran1996@gmail.com>
2021-05-29 00:28:57 +0530

    first commit

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ import sys
 
 from flask import Flask, render_template, request, session
 
-
 app = Flask(__name__)
+
 
 @app.route('/hello-world', methods=['GET'])
 def index():
     return "HELLO WORLD"
 
+
+def count():
+    if 'count' in session:
+        session['count'] = session.get('count') + 1
+    else:
+        session['count'] = 1
+
+
 @app.route('/check-palindrome', methods=['POST'])
 def palindrome():
     if request.method == 'POST':
+        count()
         request_data = request.get_json()
         value = None
         if request_data:
@@ -20,29 +29,25 @@ def palindrome():
                     v1 = request_data['value']
                     cnt = 0
                     indexes = []
-                    #return ''' value is: {}'''.format(v1)
+                    # return ''' value is: {}'''.format(v1)
                     for j in range(0, len(v1)):
                         flag = 1
-                        #fact = len(v1)
+                        # fact = len(v1)
                         data = v1[j]
                         data = data.lower()
-                        for i in range(0, int(len(data)/2)):
-                            if data[i] != data[len(data) -i - 1]:
+                        for i in range(0, int(len(data) / 2)):
+                            if data[i] != data[len(data) - i - 1]:
                                 flag = 0
                         if flag == 1:
                             cnt += 1
                             indexes.append(j)
-                    if 'count' in session:
-                        session['count'] = session.get('count') + 1
-                    else:
-                        session['count'] = 1
-                    print(session.get('count'))
                     if cnt == 0:
                         return '''No Palindromes found'''
                     else:
                         return '''{} Palindrome strings@indexes{}'''.format(cnt, indexes)
                 else:
                     return '''Array cannot be empty'''
+
 
 @app.route('/check-count', methods=['GET'])
 def get_value():
@@ -55,6 +60,6 @@ def check():
 
 
 if __name__ == '__main__':
-    app.debug=True
+    app.debug = True
     app.secret_key = 'super secret key'
     app.run()
