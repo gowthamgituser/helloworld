4fdcdf357056f4dfe5d323bdd13d60e699d0b958
gowthampsg96 <gowthamrajendran1996@gmail.com>
2021-05-29 15:15:19 +0530

    commit

--- app.py
+++ app.py
@@ -19,34 +19,20 @@ def palindrome():
             if 'value' in request_data:
                 if (type(request_data['value']) == list) and (len(request_data['value']) > 0):
                     v1 = request_data['value']
-                    cnt = 0
-                    indexes = []
-                    for j in range(0, len(v1)):
-                        flag = 1
-                        # fact = len(v1)
-                        data = v1[j]
-                        data = data.lower()
-                        for i in range(0, int(len(data) / 2)):
-                            if data[i] != data[len(data) - i - 1]:
-                                flag = 0
-                        if flag == 1:
-                            cnt += 1
-                            indexes.append(j)
-                    if cnt == 0:
-                        return '''No Palindromes found'''
-                    else:
-                        return '''{} Palindrome strings@indexes{}'''.format(cnt, indexes)
-                else:
-                    return '''Array cannot be empty'''
+                    data = v1[0]
+                    if data == " ":
+                        return '''value is empty {}'''.format(data)
 
 
 @app.route('/check-count', methods=['GET'])
 def get_value():
     return "Total visit: {}".format(session.get('count'))
 
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template('error.html'), 404
+
 
 @app.route('/')
 def check():
